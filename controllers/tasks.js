972024a713aa07a203ef8f98f880dc25d8c01d5f
ankit9835 <ankydchrismatic@gmail.com>
2022-07-06 21:36:05 +0530

    new commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -22,18 +22,48 @@ const singleTasks = async (req, res) => {
   try {
     const { id: taskId } = req.params
     const task = await Task.findOne({ _id: taskId })
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskId}` })
+    }
     res.status(200).json({ task })
   } catch (error) {
     res.status(500).json({ msg: error })
   }
 }
 
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) {
+      return res
+        .status(402)
+        .json({ msg: `task not found for the given ${taskID}` })
+    }
+    res.status(201).json({ task })
+  } catch (error) {
+    return res.status(500).json({ msg: error })
+  }
 }
 
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const remove = await Task.findOneAndDelete({ _id: taskID })
+    if (!remove) {
+      return res
+        .status(402)
+        .json({ msg: `Data not found for the given id : ${taskID}` })
+    }
+    res
+      .status(201)
+      .json({ msg: `data deleted successfully`, status: true, data: remove })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = {
